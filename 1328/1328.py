0f29f06811c13629c165def4a5d8d2780de26e4c
yunjs24 <yunjs24@gmail.com>
2022-04-28 23:59:12 +0900

    ADD : 1328

---
+++ 1328/1328.py
@@ -0,0 +1,13 @@
+import sys
+
+N, L, R = map(int, sys.stdin.readline().split())
+dp = [[[0 for _ in range(R + 1)]for _ in range(L + 1)]
+      for _ in range(N + 1)]
+dp[1][1][1] = 1
+for i in range(2, N + 1):
+    for j in range(1, L + 1):
+        for k in range(1, R + 1):
+            dp[i][j][k] = (dp[i-1][j][k]*(i-2) % 1000000007
+                           + dp[i-1][j][k-1] % 1000000007
+                           + dp[i-1][j-1][k] % 1000000007)
+print(dp[N][L][R] % 1000000007)
